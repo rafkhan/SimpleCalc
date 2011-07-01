ab4b24a3b6e4e04f93e58abc5a49c5cacbdf6758
Rafael Khan <raf.khan.f@gmail.com>
2011-07-01 00:09:01 -0400

    first commit

---
+++ src/com/rafaelkhan/simplecalc/SimpleCalcActivity.java
@@ -0,0 +1,60 @@
+package com.rafaelkhan.simplecalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SimpleCalcActivity extends Activity {
+	
+	public final int MAX_CHAR = 10; //max characters in input field
+	public String currentField; //input field
+	public Float operand1;
+	public Float operand2;
+	public Float answer;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    public void b1(View v) {
+    	
+    }
+    
+    public void b2(View v) {
+    	
+    }
+
+    public void b3(View v) {
+	
+    }
+    
+    public void b4(View v) {
+    	
+    }
+    
+    public void b5(View v) {
+    	
+    }	
+    
+    public void b6(View v) {
+	
+    }
+    
+    public void b7(View v) {
+	
+    }
+
+    public void b8(View v) {
+    	
+    }
+    
+    public void b9(View v) {
+    	
+    }
+    
+    public void b0(View v) {
+    	
+    }
+}
